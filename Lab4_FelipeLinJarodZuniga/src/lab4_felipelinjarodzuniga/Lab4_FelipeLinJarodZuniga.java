6532a3557e8adddde97c9ec122db830fef6e7b8b
Per1vai <felipelinchiu321@gmail.com>
2020-05-22 14:37:16 -0600

    Test

--- Lab4_FelipeLinJarodZuniga/src/lab4_felipelinjarodzuniga/Lab4_FelipeLinJarodZuniga.java
+++ Lab4_FelipeLinJarodZuniga/src/lab4_felipelinjarodzuniga/Lab4_FelipeLinJarodZuniga.java
@@ -1,9 +1,17 @@
 package lab4_felipelinjarodzuniga;
 
+import java.util.Scanner;
+
 public class Lab4_FelipeLinJarodZuniga {
-
+    
     public static void main(String[] args) {
-        System.out.println("hola");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Login\n2. Sign in\n3. Cancelar");
+        int menu = sc.nextInt();
+        while (menu != 3) {
+            
+        }
+        
     }
-
+    
 }
